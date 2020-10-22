c54cde5b0e40daf64d728db4a66aa5997c25929b
nekoula <nekoulahaddad@gmail.com>
2020-10-22 18:56:20 +0300

    Updates

---
+++ precache-manifest.ebc984d1dc517254d7842b3af4ab6e44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423e9eedb25513b5796361300033a4bc",
+    "url": "/https://github.com/nekoulahaddad/to-do.git/index.html"
+  },
+  {
+    "revision": "7effd9078ecc1816fbc7",
+    "url": "/https://github.com/nekoulahaddad/to-do.git/static/css/main.8d2a6d4a.chunk.css"
+  },
+  {
+    "revision": "4590869b9aac3bb02292",
+    "url": "/https://github.com/nekoulahaddad/to-do.git/static/js/2.f1d7787f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https://github.com/nekoulahaddad/to-do.git/static/js/2.f1d7787f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7effd9078ecc1816fbc7",
+    "url": "/https://github.com/nekoulahaddad/to-do.git/static/js/main.477f06e9.chunk.js"
+  },
+  {
+    "revision": "cfe59ad5434205f75a73",
+    "url": "/https://github.com/nekoulahaddad/to-do.git/static/js/runtime-main.12048274.js"
+  }
+]);
